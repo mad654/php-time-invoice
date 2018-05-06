b90ce3f4258999fd2017e40a87ad7545e8ccb12a
Matthias Mann <m.mann@caresocial.de>
2018-05-06 20:22:47 +0200

    cleanup

--- src/mad654/AtWorkTimeInvoice/FileNotFoundException.php
+++ src/mad654/AtWorkTimeInvoice/FileNotFoundException.php
@@ -6,10 +6,6 @@ namespace mad654\AtWorkTimeInvoice;
 
 class FileNotFoundException extends \Exception
 {
-
-    /**
-     * FileNotFoundException constructor.
-     */
     public function __construct(string $fileName) {
         parent::__construct("Path not exists or is no file: `not-existing-file.name`");
     }
