44231543ec4c45f60a21e0ec4b9b3b38e1f34ded
Matthias Mann <mad654@users.noreply.github.com>
2018-05-06 22:36:39 +0200

    Feature/mad654/parse at work csv exports as working hours (#1)

--- tests/mad654/AtWorkTimeInvoice/AtWorkWorkingHourTest.php
+++ tests/mad654/AtWorkTimeInvoice/AtWorkWorkingHourTest.php
@@ -20,10 +20,38 @@ class AtWorkWorkingHourTest extends TestCase
      * @test
      * @throws FileNotFoundException
      */
-    public function fromFile_always_returnsArrayWithCountFive() {
-        $actual = AtWorkWorkingHour::fromFile(__DIR__ . '/fixtures/excel-export-atwork-2018-04-30-10_36_18.csv');
+    public function fromFile_fromFixtures_returnsArrayWithFiveWorkingHourObjects() {
+        $actual = $this->instanceFromFixtures();
 
         $this->assertCount(5, $actual);
+        $this->assertInstanceOf(WorkingHour::class, $actual[0]);
+        $this->assertInstanceOf(WorkingHour::class, $actual[1]);
+        $this->assertInstanceOf(WorkingHour::class, $actual[2]);
+        $this->assertInstanceOf(WorkingHour::class, $actual[3]);
+        $this->assertInstanceOf(WorkingHour::class, $actual[4]);
+    }
+
+    /**
+     * @test
+     * @throws FileNotFoundException
+     */
+    public function fromFile_fromFixtures_firstItemWasParsedCorrectly() {
+        $expected = AtWorkWorkingHour::fromArray(array(
+            'LfdNummer' => '1',
+            'Anfang' => '2018-04-27 12:17',
+            'Ende' => '2018-04-27 16:37',
+            'Pause' => '0',
+            'Dauer' => '4,333',
+            'Stundensatz' => '36,00',
+            'Zuschlag' => '0,00',
+            'Verdienst' => '156,00',
+            'Kunde' => 'Muster ',
+            'Projekt' => 'GA',
+            'Aufgabe' => 'Fahrzeit',
+            'Notiz' => NULL,
+        ));
+
+        $this->assertEquals($expected, $this->instanceFromFixtures()[0]);
     }
 
     /**
@@ -40,5 +68,15 @@ class AtWorkWorkingHourTest extends TestCase
      */
     protected function instance(): AtWorkWorkingHour {
         return new AtWorkWorkingHour();
+    }
+
+    /**
+     * @return WorkingHour[]
+     * @throws FileNotFoundException
+     */
+    protected function instanceFromFixtures() {
+        return AtWorkWorkingHour::fromFile(
+            __DIR__ . '/fixtures/excel-export-atwork-2018-04-30-10_36_18.csv'
+        );
     }
 }
